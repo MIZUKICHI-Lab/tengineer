502faff2f1577ce63a261363e9d55584c36b4568
EngineerCafe A <engineercafea@EngineerCafenoMacBook-Air-2.local>
2022-06-17 21:46:23 +0900

    first commit

---
+++ lib/screens/mobile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MobileScreen extends StatefulWidget {
+  const MobileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MobileScreen> createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends State<MobileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
